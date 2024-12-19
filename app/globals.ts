eed3751c59b9005fd08a645023d213e9d1db9271
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-12-19 14:30:40 +0200

    added two more headers to resInit

--- app/globals.ts
+++ app/globals.ts
@@ -1,5 +1,8 @@
 export const resInit: ResponseInit = {
 	headers: {
 		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "GET, OPTIONS",
+		"Access-Control-Allow-Headers":
+			"Access-Control-Allow-Origin, Access-Control-Allow-Methods, Content-Type, Authorization",
 	},
 };
