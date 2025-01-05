f2edba5acc42c58e677848eaefe72fc9b1fc081e
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2025-01-05 12:10:40 +0200

    added importantLinks and joinus link

--- app/global/links/route.ts
+++ app/global/links/route.ts
@@ -8,31 +8,38 @@ const links: LinksDataType = {
 		{
 			href: "https://whatsapp.com/channel/0029VakgzRJ5kg7BkQFSJK36",
 			src: process.env.HOST + "/icons/whatsapp.svg",
-			alt: "whatsapp",
+			text: "whatsapp",
 		},
 		{
 			href: "https://facebook.com/mafazaa.org",
 			src: process.env.HOST + "/icons/facebook.svg",
-			alt: "facebook",
+			text: "facebook",
 		},
 		{
 			href: "https://youtube.com/@mafazaa_official",
 			src: process.env.HOST + "/icons/youtube.svg",
-			alt: "youtube",
+			text: "youtube",
 		},
 
 		{
 			href: `mailto:${supportEmail}`,
 			src: process.env.HOST + "/icons/mail.svg",
-			alt: "mail",
+			text: "mail",
 		},
 		{
 			href: "https://github.com/mafazaa-org/",
 			src: process.env.HOST + "/icons/github-mark-white.svg",
-			alt: "github logo",
+			text: "github logo",
 		},
 	],
 	support: supportEmail,
+	importantLinks: [
+		{ href: "/#projects", text: "مشاريعنا" },
+		{ href: "/contact", text: "تواصل معنا" },
+		{ href: "/idea", text: "فكرة المشروع" },
+		{ href: "/join", text: "انضم إلينا" },
+		{ href: "/", text: "الرئيسية" },
+	].map((v) => ({ href: process.env.MAIN_WEBSITE + v.href, text: v.text })),
 };
 
 export async function GET() {
@@ -46,10 +53,14 @@ export async function OPTIONS() {
 type LinksDataType = {
 	socialLinks: SocialLink[];
 	support: string;
+	importantLinks: Link[];
 };
 
-type SocialLink = {
+interface Link {
+	text: string;
 	href: string;
+}
+
+interface SocialLink extends Link {
 	src: string;
-	alt: string;
-};
+}
