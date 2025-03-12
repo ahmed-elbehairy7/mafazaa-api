213d3a350d636f85d826c3d2df55fd77f11273d6
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-03-12 15:09:18 +0200

    updated support_us route

--- app/global/links/route.ts
+++ app/global/links/route.ts
@@ -38,7 +38,7 @@ const links: LinksDataType = {
 		{ href: "/contact", text: "تواصل معنا" },
 		{ href: "/idea", text: "فكرة المشروع" },
 		{ href: "/join", text: "انضم إلينا" },
-		{ href: "/support", text: "ادعمنا" },
+		{ href: "/support_us", text: "ادعمنا" },
 		{ href: "/", text: "الرئيسية" },
 	].map((v) => ({ href: process.env.MAIN_WEBSITE + v.href, text: v.text })),
 };
