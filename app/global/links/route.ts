224122e70606d2903263ee917b541d48ef335e29
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2025-09-08 07:19:40 +0300

    updated facebook link

--- app/global/links/route.ts
+++ app/global/links/route.ts
@@ -11,7 +11,7 @@ const links: LinksDataType = {
 			text: "whatsapp",
 		},
 		{
-			href: "https://facebook.com/mafazaa.org",
+			href: "https://facebook.com/mafazaa.cic",
 			src: process.env.HOST + "/icons/facebook.svg",
 			text: "facebook",
 		},
